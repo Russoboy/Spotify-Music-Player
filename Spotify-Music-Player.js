afd2ec1ec4db75d62250d0214e67be71d3cae6d3
Daniel Adigun <105540876+Russoboy@users.noreply.github.com>
2025-02-14 10:15:21 +0100

    Create Spotify-Music-Player.js
    Spotify Music Player

---
+++ Spotify-Music-Player.js
@@ -0,0 +1,24 @@
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+const pausePlay = document.getElementById('pause-btn');
+const artistName = document.getElementById('artist-name');
+const musicName = document.getElementById('music-name');
+const musicArt = document.getElementById('music-art');
+
+
+prevBtn.addEventListener('click', () => {
+    
+})
+
+
+nextBtn.addEventListener('click', () => {
+    
+})
+
+
+pausePlay.addEventListener('click', () => {
+    
+})
+
+
+
